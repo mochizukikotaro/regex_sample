eb98ed43d2e913641518c760c5bfd4d3988786f8
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-07-27 12:08:05 +0900

    Idea with context

--- spec/regex_sample_spec.rb
+++ spec/regex_sample_spec.rb
@@ -2,11 +2,42 @@ require "spec_helper"
 
 RSpec.describe RegexSample do
   describe :generate do
+
+    # context /(a|b)/ do
+    #   it 'match' do
+    #     re = RSpec.current_example.metadata[:described_class]
+    #     expect(RegexSample.generate(re)).to match(re)
+    #   end
+    # end
+    #
+    # context /\p{katakana}/ do
+    #   it 'match' do
+    #     re = RSpec.current_example.metadata[:described_class]
+    #     expect(RegexSample.generate(re)).to match(re)
+    #   end
+    # end
+    #
+    # context /\p{hiragana}/ do
+    #   it 'match' do
+    #     re = RSpec.current_example.metadata[:described_class]
+    #     expect(RegexSample.generate(re)).to match(re)
+    #   end
+    # end
+
+    let(:re) { RSpec.current_example.metadata[:described_class] }
+    subject { RegexSample.generate(re) }
+
     context /(a|b)/ do
-      it 'match' do
-        re = RSpec.current_example.metadata[:described_class]
-        expect(RegexSample.generate(re)).to match(re)
-      end
+      it { is_expected.to match(re)  }
     end
+
+    context /\p{katakana}/ do
+      it { is_expected.to match(re)  }
+    end
+
+    context /\p{hiragana}/ do
+      it { is_expected.to match(re)  }
+    end
+
   end
 end
